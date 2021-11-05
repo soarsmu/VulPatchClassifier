7b695d091f4e434f759d588a13e29e1447fd9aad
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-11-05 11:15:16 +0800

    Change model from averaging to fully connected

--- entities.py
+++ entities.py
@@ -1,23 +1,31 @@
 from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+from transformers import RobertaTokenizer, RobertaModel
 import os
 import json
 import torch
 
 directory = os.path.dirname(os.path.abspath(__file__))
+tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+code_bert = RobertaModel.from_pretrained("microsoft/codebert-base", num_labels=2)
+empty_code = tokenizer.sep_token + ''
+inputs = tokenizer([empty_code], padding=True, max_length=512, truncation=True, return_tensors="pt")
+input_ids, attention_mask = inputs.data['input_ids'][0], inputs.data['attention_mask'][0]
+empty_embedding = code_bert(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :].tolist()
 
 
-def get_average_value(embeddings):
-    embeddings = torch.FloatTensor(embeddings)
-    sum_ = torch.sum(embeddings, dim=0)
-    mean_ = torch.div(sum_, embeddings.shape[0])
-    mean_ = mean_.detach()
-    mean_ = mean_.cpu()
-
-    return mean_
+# def get_average_value(embeddings):
+#     embeddings = torch.FloatTensor(embeddings)
+#     sum_ = torch.sum(embeddings, dim=0)
+#     mean_ = torch.div(sum_, embeddings.shape[0])
+#     mean_ = mean_.detach()
+#     mean_ = mean_.cpu()
+#
+#     return mean_
 
 
 class PatchDataset(Dataset):
     def __init__(self, list_IDs, labels, id_to_url):
+        self.max_data_length = 5
         self.list_IDs = list_IDs
         self.labels = labels
         self.id_to_url = id_to_url
@@ -33,8 +41,20 @@ class PatchDataset(Dataset):
         with open(file_path, 'r') as reader:
             data = json.loads(reader.read())
 
-        before = get_average_value(data['before'])
-        after = get_average_value(data['after'])
+        before = data['before']
+        after = data['after']
+        if len(before) > 5:
+            before = before[:5]
+        if len(after) > 5:
+            after = after[:5]
+        while len(before) < 5:
+            before.append(empty_embedding)
+        while len(after) < 5:
+            after.append(empty_embedding)
+
+        before = torch.FloatTensor(before)
+        after = torch.FloatTensor(after)
+
         y = self.labels[id]
 
         return int(id), url, before, after, y
--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from transformers import RobertaTokenizer, RobertaModel
 import torch
 from torch import nn as nn
 
@@ -7,11 +6,15 @@ class PatchClassifier(nn.Module):
     def __init__(self):
         super(PatchClassifier, self).__init__()
         self.HIDDEN_DIM = 768
-        self.linear = nn.Linear(2 * self.HIDDEN_DIM, self.HIDDEN_DIM)
+        self.before_linear = nn.Linear(5 * self.HIDDEN_DIM, self.HIDDEN_DIM)
+        self.after_linear = nn.Linear(5 * self.HIDDEN_DIM, self.HIDDEN_DIM)
+        self.combine = nn.Linear(2 * self.HIDDEN_DIM, self.HIDDEN_DIM)
         self.out_proj = nn.Linear(self.HIDDEN_DIM, 2)
 
     def forward(self, before_batch, after_batch):
-        combined = self.linear(torch.cat([before_batch, after_batch], axis=1))
+        before = self.before_linear(before_batch)
+        after = self.after_linear(after_batch)
+        combined = self.linear(torch.cat([before, after], axis=1))
         out = self.out_proj(combined)
 
         return out
