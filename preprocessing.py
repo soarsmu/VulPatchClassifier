06b8124d3550b8f88b4b9e640d6bd62237e771c0
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-11-07 21:35:13 +0800

    need eval() for codebert

--- preprocessing.py
+++ preprocessing.py
@@ -7,7 +7,8 @@ import tqdm
 from torch import cuda
 from torch import nn as nn
 
-torch.manual_seed(109)
+g = torch.Generator()
+g.manual_seed(0)
 
 directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -84,14 +85,13 @@ def write_embeddings_to_files(removed_code_list, added_code_list, url_list, toke
 def get_data():
     tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
     code_bert = RobertaModel.from_pretrained("microsoft/codebert-base", num_labels=2)
-
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
         code_bert = nn.DataParallel(code_bert)
 
     code_bert.to(device)
-
+    code_bert.eval()
     print("Reading dataset...")
     df = pd.read_csv(dataset_name)
     df = df[['commit_id', 'repo', 'partition', 'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
