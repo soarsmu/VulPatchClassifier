72314cb3aa62aff289ab2b48c0ad67a7b2377141
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-11-17 17:06:33 +0800

    Implement inference for variant 6

--- variant_6.py
+++ variant_6.py
@@ -50,14 +50,14 @@ NUMBER_OF_LABELS = 2
 model_path_prefix = model_folder_path + '/patch_variant_6_16112021_model_'
 
 
-def predict_test_data(model, testing_generator, device):
+def predict_test_data(model, testing_generator, device, need_prob=False):
     y_pred = []
     y_test = []
     probs = []
-    test_ids = []
+    urls = []
     with torch.no_grad():
         model.eval()
-        for ids, url, before_batch, after_batch, label_batch in testing_generator:
+        for ids, url_batch, before_batch, after_batch, label_batch in testing_generator:
             before_batch, after_batch, label_batch = before_batch.to(device), after_batch.to(device), label_batch.to(device)
             outs = model(before_batch, after_batch)
 
@@ -66,7 +66,7 @@ def predict_test_data(model, testing_generator, device):
             y_pred.extend(torch.argmax(outs, dim=1).tolist())
             y_test.extend(label_batch.tolist())
             probs.extend(outs[:, 1].tolist())
-            test_ids.extend(label_batch.tolist())
+            urls.extend(list(url_batch))
 
         precision = metrics.precision_score(y_pred=y_pred, y_true=y_test)
         recall = metrics.recall_score(y_pred=y_pred, y_true=y_test)
@@ -77,8 +77,10 @@ def predict_test_data(model, testing_generator, device):
             auc = 0
 
     print("Finish testing")
-    return precision, recall, f1, auc
-
+    if not need_prob:
+        return precision, recall, f1, auc
+    else:
+        return precision, recall, f1, auc, urls, probs
 
 def get_avg_validation_loss(model, validation_generator, loss_function):
     validation_loss = 0
--- variant_ensemble.py
+++ variant_ensemble.py
@@ -4,12 +4,13 @@ import os
 from torch.utils.data import Dataset, DataLoader
 from torch import cuda
 
-from entities import VariantOneDataset, VariantTwoDataset, VariantFiveDataset
-from model import VariantOneClassifier, VariantTwoClassifier, VariantFiveClassifier
+from entities import VariantOneDataset, VariantTwoDataset, VariantFiveDataset, VariantSixDataset
+from model import VariantOneClassifier, VariantTwoClassifier, VariantFiveClassifier, VariantSixClassifier
 import utils
 import variant_1
 import variant_2
 import variant_5
+import variant_6
 import csv
 # dataset_name = 'huawei_csv_subset_slicing_limited_10.csv'
 # dataset_name = 'huawei_sub_dataset.csv'
@@ -22,6 +23,7 @@ model_folder_path = os.path.join(directory, 'model')
 VARIANT_ONE_MODEL_PATH = 'model/patch_variant_1_best_model.sav'
 VARIANT_TWO_MODEL_PATH = 'model/patch_variant_2_best_model.sav'
 VARIANT_FIVE_MODEL_PATH = 'model/patch_variant_5_best_model.sav'
+VARIANT_SIX_MODEL_PATH = 'model/patch_variant_6_best_model.sav'
 
 
 TEST_BATCH_SIZE = 128
@@ -228,4 +230,65 @@ def get_variant_five_result():
     write_prob_to_file("variant_5_prob_python.txt", urls, probs)
 
 
-get_variant_five_result()
+def get_variant_six_result():
+    model = VariantSixClassifier()
+
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        model = nn.DataParallel(model)
+
+    model.to(device)
+
+    model.load_state_dict(torch.load(VARIANT_SIX_MODEL_PATH))
+
+    url_data, label_data = utils.get_data(dataset_name)
+
+    train_ids, val_ids, test_java_ids, test_python_ids = [], [], [], []
+    index = 0
+    id_to_url = {}
+    id_to_label = {}
+
+    for i, url in enumerate(url_data['test_java']):
+        test_java_ids.append(index)
+        id_to_url[index] = url
+        id_to_label[index] = label_data['test_java'][i]
+        index += 1
+
+    for i, url in enumerate(url_data['test_python']):
+        test_python_ids.append(index)
+        id_to_url[index] = url
+        id_to_label[index] = label_data['test_python'][i]
+        index += 1
+
+    test_java_set = VariantSixDataset(test_java_ids, id_to_label, id_to_url)
+    test_python_set = VariantSixDataset(test_python_ids, id_to_label, id_to_url)
+    test_java_generator = DataLoader(test_java_set, **TEST_PARAMS)
+    test_python_generator = DataLoader(test_python_set, **TEST_PARAMS)
+
+    print("Testing on Java...")
+    precision, recall, f1, auc, urls, probs = variant_6.predict_test_data(model, test_java_generator, device,
+                                                                          need_prob=True)
+
+    print("Precision: {}".format(precision))
+    print("Recall: {}".format(recall))
+    print("F1: {}".format(f1))
+    print("AUC: {}".format(auc))
+    print("-" * 32)
+
+    write_prob_to_file("variant_6_prob_java.txt", urls, probs)
+
+    print("Result on Python testing dataset...")
+    precision, recall, f1, auc, urls, probs = variant_6.predict_test_data(model=model,
+                                                                          testing_generator=test_python_generator,
+                                                                          device=device, need_prob=True)
+
+    print("Precision: {}".format(precision))
+    print("Recall: {}".format(recall))
+    print("F1: {}".format(f1))
+    print("AUC: {}".format(auc))
+    print("-" * 32)
+
+    write_prob_to_file("variant_6_prob_python.txt", urls, probs)
+
+get_variant_six_result()
