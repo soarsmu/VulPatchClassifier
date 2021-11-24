e68a0aa5742faf6e5035c977c11ad6a7b2aefd58
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-11-24 10:13:53 +0800

    update code

--- preprocess_finetuned_variant_1.py
+++ preprocess_finetuned_variant_1.py
@@ -93,7 +93,7 @@ def get_data():
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-        code_bert = nn.DataParallel(model)
+        code_bert = nn.DataParallel(code_bert)
     code_bert = code_bert.to(device)
 
     print("Reading dataset...")
