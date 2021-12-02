1a10fa64c3fc159a65895dfd5c60fa75a829ab66
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-12-02 10:57:09 +0800

    Forgot to load fine-tuned CodeBERT (used original CodeBERT instead)

--- preprocess_finetuned_variant_7.py
+++ preprocess_finetuned_variant_7.py
@@ -7,6 +7,7 @@ import tqdm
 from torch import cuda
 from torch import nn as nn
 import matplotlib.pyplot as plt
+from model import VariantSeventFineTuneOnlyClassifier
 
 directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -117,14 +118,23 @@ def get_hunk_from_diff(diff):
 
 def get_data():
     tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
-    code_bert = RobertaModel.from_pretrained("microsoft/codebert-base", num_labels=2)
+    model = VariantSeventFineTuneOnlyClassifier()
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+        model = nn.DataParallel(model)
+
+    model.load_state_dict(torch.load(FINE_TUNED_MODEL_PATH))
+    code_bert = model.module.code_bert
+
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
         code_bert = nn.DataParallel(code_bert)
+    code_bert = code_bert.to(device)
 
-    code_bert.to(device)
     code_bert.eval()
+
     print("Reading dataset...")
     df = pd.read_csv(dataset_name)
     df = df[['commit_id', 'repo', 'partition', 'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
