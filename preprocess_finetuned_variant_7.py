6182bc076120da51078a5e9372d995088760ed0a
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-12-03 00:14:10 +0800

    deal with empty hunk for preprocessing variant 7

--- preprocess_finetuned_variant_7.py
+++ preprocess_finetuned_variant_7.py
@@ -14,8 +14,8 @@ directory = os.path.dirname(os.path.abspath(__file__))
 EMBEDDING_DIRECTORY = '../finetuned_embeddings/variant_7'
 FINE_TUNED_MODEL_PATH = 'model/patch_variant_7_finetuned_model.sav'
 
-# dataset_name = 'ase_dataset_sept_19_2021.csv'
-dataset_name = 'huawei_sub_dataset.csv'
+dataset_name = 'ase_dataset_sept_19_2021.csv'
+# dataset_name = 'huawei_sub_dataset.csv'
 
 CODE_LINE_LENGTH = 256
 
@@ -62,30 +62,36 @@ def get_hunk_embeddings(code_list, tokenizer, code_bert):
     return embeddings
 
 
-def write_embeddings_to_files(removed_code_list, added_code_list, url_list, tokenizer, code_bert):
-    removed_embeddings = get_hunk_embeddings(removed_code_list, tokenizer, code_bert)
-    added_embeddings = get_hunk_embeddings(added_code_list, tokenizer, code_bert)
-
+def write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_list, added_url_list):
+    url_set = set()
     url_to_removed_embeddings = {}
-    url_to_added_embeddings = {}
-    for index, url in enumerate(url_list):
+    for index, url in enumerate(removed_url_list):
         if url not in url_to_removed_embeddings:
+            url_set.add(url)
             url_to_removed_embeddings[url] = []
-            url_to_added_embeddings[url] = []
         url_to_removed_embeddings[url].append(removed_embeddings[index])
+
+    url_to_added_embeddings = {}
+    for index, url in enumerate(added_url_list):
+        if url not in url_to_added_embeddings:
+            url_set.add(url)
+            url_to_added_embeddings[url] = []
         url_to_added_embeddings[url].append(added_embeddings[index])
 
     url_to_data = {}
-    for url, removed_embeddings in url_to_removed_embeddings.items():
-        data = {}
-        added_embeddings = url_to_added_embeddings[url]
-        data['before'] = removed_embeddings
-        data['after'] = added_embeddings
+    for url in url_set:
+        before_data = []
+        after_data = []
+        if url in url_to_removed_embeddings:
+            before_data = url_to_removed_embeddings[url]
+        if url in url_to_added_embeddings:
+            after_data = url_to_added_embeddings[url]
+
+        data = {'before': before_data, 'after': after_data}
         url_to_data[url] = data
     for url, data in url_to_data.items():
         file_path = os.path.join(directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
         json.dump(data, open(file_path, 'w'))
-
 
 def hunk_empty(hunk):
     if hunk.strip() == '':
@@ -155,70 +161,39 @@ def get_data():
 
     removed_code_list = []
     added_code_list = []
-    url_list = []
+    removed_url_list = []
+    added_url_list = []
     for url, diff_list in tqdm.tqdm(url_to_hunk.items()):
         file_path = os.path.join(directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
         if os.path.isfile(file_path):
             continue
 
         for i, diff in enumerate(diff_list):
-            removed_code = tokenizer.sep_token + get_code_version(diff, False)
-            added_code = tokenizer.sep_token + get_code_version(diff, True)
+            removed_code = get_code_version(diff, False)
+            if removed_code.strip() != '':
+                removed_code_list.append(removed_code)
+                removed_url_list.append(url)
 
-            removed_code_list.append(removed_code)
-            added_code_list.append(added_code)
-            url_list.append(url)
+            added_code = get_code_version(diff, True)
+            if added_code.strip() != '':
+                added_code_list.append(added_code)
+                added_url_list.append(url)
 
-        if len(url_list) >= 200:
-            write_embeddings_to_files(removed_code_list, added_code_list, url_list, tokenizer, code_bert)
+        if len(removed_code_list) >= 50 or len(added_code_list) >= 50:
+            removed_embeddings = get_hunk_embeddings(removed_code_list, tokenizer, code_bert)
+            added_embeddings = get_hunk_embeddings(added_code_list, tokenizer, code_bert)
+
+            write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_list, added_url_list)
 
             removed_code_list = []
             added_code_list = []
-            url_list = []
+            removed_url_list = []
+            added_url_list = []
 
-    write_embeddings_to_files(removed_code_list, added_code_list, url_list, tokenizer, code_bert)
-
-
-def get_token_count(code, tokenizer):
-    inputs = tokenizer(code, return_tensors="pt")
-    token_count = inputs.data['input_ids'].shape[1]
-
-    return token_count
-
-
-def get_hunk_token_count_distribution():
-    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
-    code_bert = RobertaModel.from_pretrained("microsoft/codebert-base", num_labels=2)
-    if torch.cuda.device_count() > 1:
-        print("Let's use", torch.cuda.device_count(), "GPUs!")
-        # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-        code_bert = nn.DataParallel(code_bert)
-
-    code_bert.to(device)
-    code_bert.eval()
-    print("Reading dataset...")
-    df = pd.read_csv(dataset_name)
-    df = df[df.label == 1]
-    df = df[['diff']]
-
-    hunk_list = []
-    for item in df.values.tolist():
-        diff = item[0]
-        hunk_list.extend(get_hunk_from_diff(diff))
-
-    added_token_count = []
-    removed_token_count = []
-    for hunk in tqdm.tqdm(hunk_list):
-        removed_code = tokenizer.sep_token + get_code_version(hunk, False)
-        added_code = tokenizer.sep_token + get_code_version(hunk, True)
-        added_token_count.append(get_token_count(added_code, tokenizer))
-        removed_token_count.append(get_token_count(removed_code, tokenizer))
-
-    plt.boxplot([added_token_count, removed_token_count],
-                labels=['Added_token_count', "Removed_token_count"], showfliers=True)
-    plt.title("Distribution in token count by hunk")
-    plt.ylabel("No. Token count")
-    plt.show()
+    if len(removed_code_list) > 0 or len(added_code_list) > 0:
+        removed_embeddings = get_hunk_embeddings(removed_code_list, tokenizer, code_bert)
+        added_embeddings = get_hunk_embeddings(added_code_list, tokenizer, code_bert)
+        write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_list, added_url_list)
 
 
 if __name__ == '__main__':
