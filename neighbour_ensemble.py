a66bb84b409bc1aade1eb8fe4b384720690704ff
ntgiang71096 <nguyentruonggiang71096@gmail.com>
2021-12-10 16:34:23 +0800

    update code

--- neighbour_ensemble.py
+++ neighbour_ensemble.py
@@ -102,14 +102,13 @@ def process():
 
     count = 0
     for i, url in enumerate(url_data['test_java']):
-        if label_data['test_java'][i] == 1:
-            count += 1
-            if count % 100 == 0:
-                print("finish: {}/{}".format(count, len(url_data['test_java'])))
-            # print(url)
-            url_to_neighbor[url] = find_neighbour(url, url_to_features, url_data, label_data, url_to_pl)
-            # if count == 10:
-            #     break
+        count += 1
+        if count % 100 == 0:
+            print("finish: {}/{}".format(count, len(url_data['test_java'])))
+        # print(url)
+        url_to_neighbor[url] = find_neighbour(url, url_to_features, url_data, label_data, url_to_pl)
+        # if count == 10:
+        #     break
     json.dump(url_to_neighbor, open('url_to_neighbour_java.txt', 'w'))
 
 
